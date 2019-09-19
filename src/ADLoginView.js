a72c37be68fdd65addae7127c9a13548c8ed4a74
pavanakshay <pavanakshay502@gmail.com>
2019-09-19 12:14:37 -0400

    Update ADLoginView.js

--- src/ADLoginView.js
+++ src/ADLoginView.js
@@ -1,6 +1,7 @@
 // @flow
 import React, {Component} from 'react'
-import {WebView, Dimensions, AsyncStorage, Platform} from 'react-native'
+import {Dimensions, AsyncStorage, Platform} from 'react-native'
+import WebView from 'react-native-webview'
 import CONST from './const.js'
 import ReactNativeAD from './ReactNativeAD.js'
 import Timer from 'react-timer-mixin'
